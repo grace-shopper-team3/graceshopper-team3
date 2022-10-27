779b5e6142b4c63cb6566d2b07679b991c529522
Tasnim Mollah <tasnmmollah@gmail.com>
2022-10-27 13:50:35 -0400

    Issue#45 & backend

--- client/features/singleProduct/singleProductSlice.js
+++ client/features/singleProduct/singleProductSlice.js
@@ -1,22 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchSingleProduct = createAsyncThunk("fetchProduct", async (productId) => {
-  const { data } = await axios.get(`/api/products/${productId}`); //check w backend route
-  return data;
-});
+export const fetchSingleProduct = createAsyncThunk(
+  "fetchProduct",
+  async (productId) => {
+    const { data } = await axios.get(`/api/products/${productId}`);
+    return data;
+  }
+);
 
 const singleProductSlice = createSlice({
   name: "singleProduct",
   initialState: {
-    singleProduct: {},
+    aProduct: {},
     error: null,
   },
   reducers: {},
   extraReducers(builder) {
     builder
       .addCase(fetchSingleProduct.fulfilled, (state, action) => {
-        state.singleProduct = action.payload;
+        state.aProduct = action.payload;
       })
       .addCase(fetchSingleProduct.rejected, (state, action) => {
         state.error = action.error;
---
+++ server/api/orderRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const {
+  models: { User, Order, Order_Product },
+} = require("../db");
+
+module.exports = router;
