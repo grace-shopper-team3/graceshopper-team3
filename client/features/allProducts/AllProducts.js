4c01cbeaf42154e9a9a75a63115fa1f6b903e5d6
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-10-28 16:09:06 -0400

    User&Product seed data

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -4,10 +4,6 @@ import { fetchAllProducts } from "./allProductsSlice";
 import { Link } from "react-router-dom";
 
 const AllProducts = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  const { id } = useSelector((state) => state.auth.me);
-  console.log(id);
-
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAllProducts());
@@ -28,115 +24,59 @@ const AllProducts = () => {
           All PunkoFops
         </h1>
       </section>
-      {isLoggedIn ? (
-        <section>
-          <div className="container">
-            <div className={"row"}>
-              {allproducts.map((product) => (
+      <section>
+        <div className="container">
+          <div className={"row"}>
+            {allproducts.map((product) => (
+              <div
+                key={product.id}
+                className="col-sm-4"
+                style={{
+                  marginTop: `30px`,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 <div
-                  key={product.id}
-                  className="col-sm-4"
+                  className="card border-secondary"
                   style={{
-                    marginTop: `30px`,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    width: `18rem`,
+                    height: `25rem`,
+                    display: `flex`,
+                    justifyContent: `center`,
+                    alignItems: `center`,
                   }}
                 >
-                  <div
-                    className="card border-secondary"
-                    style={{
-                      width: `18rem`,
-                      height: `25rem`,
-                      display: `flex`,
-                      justifyContent: `center`,
-                      alignItems: `center`,
-                    }}
-                  >
-                    <Link to={`/${id}/products/${product.id}`}>
-                      <div
-                        style={{
-                          width: `14rem`,
-                          height: `16rem`,
-                          backgroundImage: `url(${product.imageUrl})`,
-                          backgroundSize: `cover`,
-                        }}
-                        className="card-img-top"
-                      ></div>
-                      <h5
-                        className="card-title text-center"
-                        style={{ color: `black` }}
-                      >
-                        {product.name}
-                      </h5>
+                  <Link to={`/products/${product.id}`}>
+                    <div
+                      style={{
+                        width: `14rem`,
+                        height: `16rem`,
+                        backgroundImage: `url(${product.imageUrl})`,
+                        backgroundSize: `cover`,
+                      }}
+                      className="card-img-top"
+                    ></div>
+                    <h5
+                      className="card-title text-center"
+                      style={{ color: `black` }}
+                    >
+                      {product.name}
+                    </h5>
+                  </Link>
+                  <div className="card-body text-center">
+                    <p>${product.price}</p>
+                    <Link href="#" className="btn btn-primary">
+                      ADD TO CART
                     </Link>
-                    <div className="card-body text-center">
-                      <p>${product.price}</p>
-                      <Link href="#" className="btn btn-primary">
-                        ADD TO CART
-                      </Link>
-                    </div>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        </section>
-      ) : (
-        <section>
-          <div className="container">
-            <div className={"row"}>
-              {allproducts.map((product) => (
-                <div
-                  key={product.id}
-                  className="col-sm-4"
-                  style={{
-                    marginTop: `30px`,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <div
-                    className="card border-secondary"
-                    style={{
-                      width: `18rem`,
-                      height: `25rem`,
-                      display: `flex`,
-                      justifyContent: `center`,
-                      alignItems: `center`,
-                    }}
-                  >
-                    <Link to={`/products/${product.id}`}>
-                      <div
-                        style={{
-                          width: `14rem`,
-                          height: `16rem`,
-                          backgroundImage: `url(${product.imageUrl})`,
-                          backgroundSize: `cover`,
-                        }}
-                        className="card-img-top"
-                      ></div>
-                      <h5
-                        className="card-title text-center"
-                        style={{ color: `black` }}
-                      >
-                        {product.name}
-                      </h5>
-                    </Link>
-                    <div className="card-body text-center">
-                      <p>${product.price}</p>
-                      <Link href="#" className="btn btn-primary">
-                        ADD TO CART
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
-      )}
+        </div>
+      </section>
     </div>
     </div>
   );
