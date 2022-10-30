2cee74822d67f5f697b3f4a09b713ca224d8887a
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-10-30 18:50:21 -0400

    empty

---
+++ client/features/cart/EmptyCart.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmptyCart = () => {
+  return (
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-body cart">
+              <div class="col-sm-12 empty-cart-cls text-center">
+                <img
+                  src="https://i.imgur.com/OgIZ3P3.png"
+                  width="330"
+                  height="330"
+                  class="img-fluid mb-4 mr-3"
+                />
+                <h3>
+                  <strong>Your Cart is Empty</strong>
+                </h3>
+                {/* <h4>Add something to make me happy :)</h4> */}
+                <Link
+                  to="/products"
+                  class="btn btn-primary cart-btn-transform m-3"
+                  data-abc="true"
+                >
+                  continue shopping
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyCart;
