1319f6d8b0f21d16c072405f4b277db71895a880
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-10-31 19:17:45 -0400

    updated cart and checkout components

--- client/features/cart/EmptyCart.js
+++ client/features/cart/EmptyCart.js
@@ -6,16 +6,15 @@ const EmptyCart = () => {
     <div>
       <section className="vh-100">
         <div className="container py-5 h-100">
-          <div class="row">
-            <div class="col-md-12">
-              {/* <div class="card"> */}
-              <div class="card-body cart">
-                <div class="col-sm-12 empty-cart-cls text-center">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card-body cart">
+                <div className="col-sm-12 empty-cart-cls text-center">
                   <img
                     src="https://i.imgur.com/OgIZ3P3.png"
                     width="330"
                     height="330"
-                    class="img-fluid mb-4 mr-3"
+                    className="img-fluid mb-4 mr-3"
                   />
                   <h3>
                     <strong>Your Cart is Empty</strong>
@@ -23,7 +22,7 @@ const EmptyCart = () => {
 
                   <Link
                     to="/products"
-                    class="btn btn-primary cart-btn-transform m-3"
+                    className="btn btn-primary cart-btn-transform m-3"
                     data-abc="true"
                   >
                     Continue Shopping
