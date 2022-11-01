b641cfee5cfcc2c60ae266fb82635a7dfba21fa3
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2022-10-31 22:02:47 -0400

    redid header added hover color change

--- client/features/navbar/LoggedInNavbar.js
+++ client/features/navbar/LoggedInNavbar.js
@@ -9,7 +9,7 @@ const LoggedInNavbar = () => {
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate("/home");
+    // navigate("/home");
   };
   const { name } = useSelector((state) => state.auth.me);
   const capitalizeFirst = (str) => {
@@ -17,25 +17,33 @@ const LoggedInNavbar = () => {
   };
 
   return (
-    <form className="navbar-form navbar-left">
-      <small>Welcome back, {capitalizeFirst(name)}! </small>
-      <button className="btn btn-link" onClick={logoutAndRedirectHome}>
-        Logout
-      </button>
-
-      <Link to="/cart">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          className="bi bi-cart"
-          viewBox="0 0 16 16"
+    <>
+      <li class="nav-item">
+        <Link
+          class="nav-link active"
+          onClick={logoutAndRedirectHome}
+          aria-current="page"
+          to="/home"
         >
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </svg>
-      </Link>
-    </form>
+          Logout
+        </Link>
+      </li>
+
+      <li class="nav-item justify-content-end">
+        <Link class="nav-link active" aria-current="page" to="/cart">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-cart"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+          </svg>
+        </Link>
+      </li>
+    </>
   );
 };
 
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -10,23 +10,42 @@ const Navbar = () => {
   const cart = useSelector((state) => state.cart.cart);
 
   return (
-    <div className="sticky-top">
-      <nav className="navbar sticky-top navbar-light bg-light">
-        <div className="container-fluid">
-          <div className="navbar-header ">
-            <Link to="/">
-              <img
-                style={{ width: 70, height: 40 }}
-                src="https://i.imgur.com/gxa6IWX.png"
-              />
-            </Link>
-            <Link to="/">Home</Link>
-            <Link to="/products">All Products</Link>
-          </div>
-          {isLoggedIn ? <LoggedInNavbar /> : <NotLoggedInNavbar />}
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+      <div class="container-fluid">
+        <Link to="/">
+          <img
+            style={{ width: 70, height: 40 }}
+            src="https://i.imgur.com/gxa6IWX.png"
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/home">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/products">
+                Products
+              </Link>
+            </li>
+            {isLoggedIn ? <LoggedInNavbar /> : <NotLoggedInNavbar />}
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
